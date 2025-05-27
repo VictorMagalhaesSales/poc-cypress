d72ec75a1e078720d2c2277ea5205374705db24d
Victor Magalhães <victormagalhaessales@gmail.com>
2025-05-27 08:52:03 -0300

    Accessing fixtures in simple test

--- cypress/e2e/simple-tests/simple-test.cy.js
+++ cypress/e2e/simple-tests/simple-test.cy.js
@@ -3,13 +3,22 @@
 describe('Simple test app', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
+    cy.fixture('simple-test.json').as('simpleTestData')
   })
 
   it('displays two todo items by default', () => {
-    cy.get('input.App-input').type('Buy groceries')
-    cy.get('button.App-link').click()
-    cy.on('window:alert', (text) => {
-      expect(text).to.contains('Button clicked with input: Buy groceries')
+
+    cy.get('@simpleTestData').then((testData) => {
+
+      cy.get('input.App-input').type(testData.inputValue)
+  
+      cy.get('button.App-link').click()
+  
+      cy.on('window:alert', (text) => {
+        expect(text).to.contains(testData.alertMessage)
+      })
+      
     })
+
   })
 })
